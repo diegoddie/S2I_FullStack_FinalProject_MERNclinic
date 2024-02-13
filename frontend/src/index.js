ad09742580419b99803f64e609b70c233548bc4c
Diego <diego.boost@gmail.com>
2024-02-13 21:40:52 +0100

    minor changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AuthContextProvider } from './context/AuthContext.js';
+import { AuthContextProvider } from './context/AuthContext';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
